bc5ae5fe7b4e95e4bdb2d657f279b5518350983b
zakeergit <mdzakeerhussain8@gmail.com>
2022-06-30 12:01:04 +0530

    update

---
+++ src/Goodnight.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import App from "./App";
+
+class Goodnight extends Component(){
+    render(){
+        return(
+            <div>
+                <h1>Hello React</h1>
+                <h5>Welcome to React World!</h5>
+                <p>We are starting new Technology</p>
+            </div>
+        );
+        
+    }
+}
+
+export default Goodnight
